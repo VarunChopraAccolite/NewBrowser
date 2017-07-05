691c7fc197504f3e86f1666e64dc1682bf2e30b5
VarunChopraAccolite <varun.chopra@accolitelabs.com>
2017-07-05 19:53:26 +0530

    Add project files.

---
+++ NewBrowser/CefCustomObject.cs
@@ -0,0 +1,50 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace NewBrowser
+{
+    class CefCustomObject
+    {
+        // Declare a local instance of chromium and the main form in order to execute things from here in the main thread
+        private static ChromiumWebBrowser _instanceBrowser = null;
+
+        // The form class needs to be changed according to yours
+        private static Form1 _instanceMainForm = null;
+
+        public CefCustomObject(ChromiumWebBrowser originalBrowser, Form1 mainForm)
+        {
+            _instanceBrowser = originalBrowser;
+            _instanceMainForm = mainForm;
+        }
+
+        public void showDevTools()
+        {
+            if (_instanceMainForm.isBrowserInitialized)
+            {
+                _instanceBrowser.ShowDevTools();
+            }
+        }
+
+        public void opencmd()
+        {
+            ProcessStartInfo start = new ProcessStartInfo("cmd.exe", "/c pause");
+            Process.Start(start);
+        }
+
+        public void openGoogle()
+        {
+            // start web url
+            var startUrl = "http://google.com";
+            _instanceBrowser.Load(startUrl);
+        }
+
+        public void openDetectMyBrowser()
+        {
+            // start web url
+            var startUrl = "http://detectmybrowser.com/";
+            _instanceBrowser.Load(startUrl);
+        }
+    }
+}
---
+++ NewBrowser/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace NewBrowser
+{
+    public partial class Form1 : Form
+    {
+        public ChromiumWebBrowser chromeBrowser;
+        public bool isBrowserInitialized;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            // At the initialization start chromium
+            InitializeChromium();
+
+            // Register an object in javascript named "cefCustomObject" with function of the CefCustomObject class :3
+            chromeBrowser.RegisterJsObject("cefCustomObject", new CefCustomObject(chromeBrowser, this));
+
+            // Check browser is Initialized
+            chromeBrowser.IsBrowserInitializedChanged += ChromeBrowser_IsBrowserInitializedChanged;
+        }
+
+        private void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+            
+            // Initialize Cef with the provided Settings
+            Cef.Initialize(settings);
+            
+            // Now instead use startUrl as URL we'll use the "page" variable to load our local resource
+            var page = $@"{Application.StartupPath}\html-resources\html\index.html";
+
+            if (!File.Exists(page))
+            {
+                MessageBox.Show("Error The html file doesn't exists : " + page);
+            }
+
+            // Create a browser component and provide start url
+            chromeBrowser = new ChromiumWebBrowser(page);
+
+            // Add the browser to the form
+            this.Controls.Add(chromeBrowser);
+
+            // Make the browser fill the form
+            chromeBrowser.Dock = DockStyle.Fill;
+
+            // Allow the use of local resources in the browser
+            BrowserSettings browserSettings = new BrowserSettings();
+            browserSettings.FileAccessFromFileUrls = CefState.Enabled;
+            browserSettings.UniversalAccessFromFileUrls = CefState.Enabled;
+            chromeBrowser.BrowserSettings = browserSettings;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void ChromeBrowser_IsBrowserInitializedChanged(object sender, IsBrowserInitializedChangedEventArgs e)
+        {
+            isBrowserInitialized = e.IsBrowserInitialized;
+        }
+    }
+}
